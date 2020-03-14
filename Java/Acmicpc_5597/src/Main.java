c8edaa9c777b8da27dbc74a9024dfdcf0e46489d
Arasia <rlacndtjs261@gmail.com>
2020-03-15 01:24:43 +0900

    Acmicpc - 과제 안 내신분..? (https://www.acmicpc.net/problem/5597)

---
+++ Java/Acmicpc_5597/src/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/*
+Acmicpc
+- 과제 안 내신분..? (https://www.acmicpc.net/problem/5597)
+ */
+public class Main {
+    public static void main(String[] args) {
+        try (
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
+        ) {
+            boolean[] check = new boolean[31];
+
+            for (int i = 0; i < 28; i++) {
+                check[Integer.parseInt(br.readLine())] = true;
+            }
+
+            for (int i = 1; i < 31; i++) {
+                if(!check[i]) {
+                    bw.write(i + "\n");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
