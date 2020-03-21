59c60a371ca15f3c19df6b53f9a875ad866c6174
Kim Choung Sun <31440205+Arasia@users.noreply.github.com>
2020-03-21 23:17:32 +0900

    - 별찍기 - 13 (https://www.acmicpc.net/problem/2523)

---
+++ Java/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/*
+Acmicpc
+- 별찍기 - 13 (https://www.acmicpc.net/problem/2523)
+ */
+public class Main {
+    public static void main(String[] args) {
+        try (
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
+        ) {
+            int N = Integer.parseInt(br.readLine());
+
+            for (int i = 1; i <= N; i++) {
+                bw.write("*".repeat(i) + "\n");
+            }
+
+            for (int i = N-1; i > 0; i--) {
+                bw.write("*".repeat(i) + "\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
