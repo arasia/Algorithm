d315dc89bb5f0b76d15a2a5ece85a0c940590c23
Arasia <rlacndtjs261@gmail.com>
2020-03-22 17:48:01 +0900

    Acmicpc - 최대공약수 (https://www.acmicpc.net/problem/1850)

---
+++ Java/Acmicpc_1850/src/Main.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/*
+Acmicpc
+- 최대공약수 (https://www.acmicpc.net/problem/1850)
+ */
+public class Main {
+    public static void main(String[] args) {
+        try (
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
+        ) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long A = Long.parseLong(st.nextToken());
+            long B = Long.parseLong(st.nextToken());
+
+            long result = GCD(A, B);
+
+            bw.write("1".repeat((int) result));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static long GCD(long a, long b) {
+        if (a % b == 0) {
+            return b;
+        }
+
+        return GCD(b, a % b);
+    }
+}
