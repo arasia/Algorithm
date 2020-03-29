6b6365656a973db6df9de17a2c8ccbf1b6d832f1
Arasia <rlacndtjs261@gmail.com>
2020-03-30 02:04:45 +0900

    Acmicpc - 단어 길이 재기 (https://www.acmicpc.net/problem/2743)

---
+++ Java/Acmicpc_2743/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/*
+Acmicpc
+- 단어 길이 재기 (https://www.acmicpc.net/problem/2743)
+ */
+public class Main {
+    public static void main(String[] args) {
+        String text = new Scanner(System.in).nextLine();
+        System.out.println(text.length());
+    }
+}
