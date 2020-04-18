edf7551166d54789fbca39b2b56a58fa1f7fd142
Arasia <rlacndtjs261@gmail.com>
2020-04-18 16:08:51 +0900

    Acmicpc 10926
    - ??! (https://www.acmicpc.net/problem/10926)

---
+++ Java/Acmicpc_10926/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/*
+Acmicpc 10926
+
+- ??! (https://www.acmicpc.net/problem/10926)
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next() + "??!");
+    }
+}
