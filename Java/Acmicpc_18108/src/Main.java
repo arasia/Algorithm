fb1db462d9aec51ad7d2dfcf1c9421d6c35a5525
Arasia <rlacndtjs261@gmail.com>
2020-06-05 00:05:23 +0900

    Acmicpc 18108
    - 1009년생인 내가 태국에서는 2541년생?! (https://www.acmicpc.net/problem/18108)

---
+++ Java/Acmicpc_18108/src/Main.java
@@ -0,0 +1,10 @@
+/*
+Acmicpc 18108
+
+- 1009년생인 내가 태국에서는 2541년생?! (https://www.acmicpc.net/problem/18108)
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new java.util.Scanner(System.in).nextInt() - 543);
+    }
+}
