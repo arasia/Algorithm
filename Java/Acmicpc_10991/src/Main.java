d684e7acf0253f57d6e6f0395fd53f6de498cc0e
Arasia <rlacndtjs261@gmail.com>
2020-06-17 00:05:51 +0900

    Acmicpc 10991
    - 별 찍기 - 16 (https://www.acmicpc.net/problem/10991)

---
+++ Java/Acmicpc_10991/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+/*
+Acmicpc 10991
+
+- 별 찍기 - 16 (https://www.acmicpc.net/problem/10991)
+*/
+public class Main {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        IntStream.rangeClosed(1, n)
+                .forEach(i ->
+                        sb.append(" ".repeat(n - i))
+                                .append("* ".repeat(i - 1))
+                                .append("*\n")
+                );
+
+        System.out.println(sb.toString());
+    }
+}
