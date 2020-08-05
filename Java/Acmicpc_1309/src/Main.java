a1857e8e3803ba1b4b1862c64754303b316d7c69
Arasia <rlacndtjs261@gmail.com>
2020-08-06 00:09:20 +0900

    Acmicpc 1309
    - 동물원 (https://www.acmicpc.net/problem/1309)

---
+++ Java/Acmicpc_1309/src/Main.java
@@ -0,0 +1,20 @@
+/*
+Acmicpc 1309
+
+- 동물원 (https://www.acmicpc.net/problem/1309)
+*/
+public class Main {
+    public static void main(String[] args) {
+        int N = new java.util.Scanner(System.in).nextInt();
+        int[][] dp = new int[N+1][3];
+        java.util.Arrays.fill(dp[1], 1);
+
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % 9901;
+        }
+
+        System.out.println((dp[N][0] + dp[N][1] + dp[N][2]) % 9901);
+    }
+}
