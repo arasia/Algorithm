a7dd507b198a88036def7f3f2792e79e450f08a6
Arasia <rlacndtjs261@gmail.com>
2020-08-24 21:33:25 +0900

    Acmicpc 5532
    - 방학 숙제 (https://www.acmicpc.net/problem/5532)

---
+++ Java/Acmicpc_5532/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/*
+Acmicpc 5532
+
+- 방학 숙제 (https://www.acmicpc.net/problem/5532)
+*/
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int L = sc.nextInt();
+        int A = sc.nextInt(), B = sc.nextInt();
+        int C = sc.nextInt(), D = sc.nextInt();
+
+        int m = (A % C) == 0 ? A / C : A / C + 1;
+        int k = (B % D) == 0 ? B / D : B /D + 1;
+
+        System.out.println(L - Math.max(m, k));
+    }
+}
