15ce75efc9a8584a62201c50a6bcbaecb7754a6f
Arasia <rlacndtjs261@gmail.com>
2020-08-29 00:17:45 +0900

    Acmicpc 16483
    - 접시 안의 원 (https://www.acmicpc.net/problem/16483)

---
+++ Java/Acmicpc_16483/src/Main.java
@@ -0,0 +1,10 @@
+/*
+Acmicpc 16483
+
+- 접시 안의 원 (https://www.acmicpc.net/problem/16483)
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Math.round(Math.pow(new java.util.Scanner(System.in).nextDouble() / 2, 2)));
+    }
+}
