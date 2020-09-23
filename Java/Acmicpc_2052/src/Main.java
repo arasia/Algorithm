d919fbdd0821131872ac816e0365c2ff0aa06642
Arasia <rlacndtjs261@gmail.com>
2020-09-24 02:31:47 +0900

    Acmicpc 2052
    - 지수연산 (https://www.acmicpc.net/problem/2052)

---
+++ Java/Acmicpc_2052/src/Main.java
@@ -0,0 +1,19 @@
+import java.math.BigDecimal;
+
+/*
+Acmicpc 2052
+
+- 지수연산 (https://www.acmicpc.net/problem/2052)
+*/
+public class Main {
+    public static void main(String[] args) {
+        int N = new java.util.Scanner(System.in).nextInt();
+        BigDecimal result = new BigDecimal("1");
+
+        while (N-- > 0) {
+            result = result.divide(new BigDecimal("2"));
+        }
+
+        System.out.println(result.toPlainString());
+    }
+}
