a7555b0d62855e7f19430021d85d3d9750b2d333
Arasia <rlacndtjs261@gmail.com>
2020-11-23 22:43:21 +0900

    Acmicpc 10420
    - 기념일 1 (https://www.acmicpc.net/problem/10420)

---
+++ Java/Acmicpc_10420/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Calendar;
+
+/*
+Acmicpc 10420
+
+- 기념일 1 (https://www.acmicpc.net/problem/10420)
+*/
+public class Main {
+    public static void main(String[] args) {
+        Calendar cal =  Calendar.getInstance();
+        cal.set(2014, Calendar.APRIL, 2);
+        cal.add(Calendar.DATE, new java.util.Scanner(System.in).nextInt() - 1);
+        System.out.println(new java.text.SimpleDateFormat("yyyy-MM-dd").format(cal.getTime()));
+    }
+}
