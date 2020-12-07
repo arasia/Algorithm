8dd748aa1853dda0bf5d1a1f028ff4f23a75b9ae
Arasia <rlacndtjs261@gmail.com>
2020-12-07 20:36:31 +0900

    Acmicpc 19944
    - 뉴비의 기준은 뭘까? (https://www.acmicpc.net/problem/19944)

---
+++ Java/Acmicpc_19944/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/*
+Acmicpc 19944
+
+- 뉴비의 기준은 뭘까? (https://www.acmicpc.net/problem/19944)
+*/
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(), M = sc.nextInt();
+
+        if (M <= N) {
+            if (M < 3) {
+                System.out.println("NEWBIE!");
+            } else {
+                System.out.println("OLDBIE!");
+            }
+        } else {
+            System.out.println("TLE!");
+        }
+    }
+}
