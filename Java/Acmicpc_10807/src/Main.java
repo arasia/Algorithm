c7a8c2db1ed30dc75849751c31846d5e326a55b8
Arasia <rlacndtjs261@gmail.com>
2020-12-25 19:10:52 +0900

    Acmicpc 10807
    - 개수 세기 (https://www.acmicpc.net/problem/10807)

---
+++ Java/Acmicpc_10807/src/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+/*
+Acmicpc 10807
+
+- 개수 세기 (https://www.acmicpc.net/problem/10807)
+*/
+public class Main {
+    public static void main(String[] args) {
+        try (
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
+        ) {
+            int N = Integer.parseInt(br.readLine());
+            Map<Integer, Integer> map = new HashMap<>();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < N; i++) {
+                int current = Integer.parseInt(st.nextToken());
+                map.put(current, map.computeIfAbsent(current, integer -> 0) + 1);
+            }
+
+            bw.write(Integer.toString(map.computeIfAbsent(Integer.parseInt(br.readLine()), integer -> 0)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
